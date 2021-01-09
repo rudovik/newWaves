139b04aaea55e2238a52261ea36d1a83ffcb9100
rudovik <giguni@ukr.net>
2021-01-09 23:37:21 +0200

    Dashboard component created.

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,10 +5,13 @@ import Layout from './hoc/Layout'
 import RegisterLogin from './components/RegisterLogin/RegisterLogin'
 import Register from './components/RegisterLogin/Register'
 
+import UserDashboard from './components/User/UserDashboard'
+
 function Routes() {
   return (
     <Layout>
       <Switch>
+        <Route path='/user/dashboard' exact component={UserDashboard} />
         <Route path='/register' exact component={Register} />
         <Route path='/register_login' exact component={RegisterLogin} />
         <Route path='/' exact component={Home} />
